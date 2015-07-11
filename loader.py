1ae46542b68867a7603eb546f42916492f1a48d1
CookieCompiler <christian_tessnow@yahoo.de>
2015-07-11 11:58:47 +0200

    reformated code

--- loader.py
+++ loader.py
@@ -8,8 +8,8 @@ import os
 import urllib3
 
 
-ts = str(round(time.time()*1000))
-url = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc="+ts
+ts = str(round(time.time() * 1000))
+url = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=" + ts
 http = urllib3.PoolManager()
 res = http.request('GET', url).data.decode("utf-8")
 j = json.loads(res)
@@ -17,5 +17,4 @@ imageurl = "http://www.bing.com" + j['images'][0]['url']
 image = http.request('GET', imageurl)
 if not os.path.isdir("images"):
     os.makedirs("images")
-
 open("images/" + ts + ".jpg", 'wb').write(image.data)
