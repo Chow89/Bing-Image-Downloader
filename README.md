4d636336b4c0a3f04554deed95623b2d30e328be
CookieCompiler <christian_tessnow@yahoo.de>
2015-07-11 11:36:48 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Bing-Image-Downloader
-downloads the current bing.com background image
+This script downloads the current bing.com background image.
+
+For executing the script regularly use crontab and insert the following line
+<code>0 0 * * * /path/to/file/loader.py</code>
+to download the image every day at midnight.
---
+++ loader.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import json
+import os
+
+import urllib3
+
+
+ts = str(round(time.time()*1000))
+url = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc="+ts
+http = urllib3.PoolManager()
+res = http.request('GET', url).data.decode("utf-8")
+j = json.loads(res)
+imageurl = "http://www.bing.com" + j['images'][0]['url']
+image = http.request('GET', imageurl)
+if not os.path.isdir("images"):
+    os.makedirs("images")
+
+open("images/" + ts + ".jpg", 'wb').write(image.data)
