2c7ba6643f6a603a886c9b9a67f4e8629118b2b5
CookieCompiler <christian_tessnow@yahoo.de>
2015-07-11 12:40:54 +0200

    edited filename of saved image

--- loader.py
+++ loader.py
@@ -13,8 +13,11 @@ url = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=" + ts
 http = urllib3.PoolManager()
 res = http.request('GET', url).data.decode("utf-8")
 j = json.loads(res)
-imageurl = "http://www.bing.com" + j['images'][0]['url']
+imageurl = ("http://www.bing.com" + j['images'][0]['url'])
+imageurlsplitted = imageurl.split("/")
+imageurlsplitted.reverse()
+filename = imageurlsplitted[0]
 image = http.request('GET', imageurl)
 if not os.path.isdir("images"):
     os.makedirs("images")
-open("images/" + ts + ".jpg", 'wb').write(image.data)
+open("images/" + filename, 'wb').write(image.data)
